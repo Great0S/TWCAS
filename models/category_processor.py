dac6c86e3d902f1e89e37808929df4e843d2d145
GreatOs <greatos@outlook.com>
2023-01-07 13:03:34 +0300

    updates

--- models/category_processor.py
+++ models/category_processor.py
@@ -1,29 +1,45 @@
+import re
 from config.settings import settings
 from tasks.erros_notify import feedback
 
 logger = settings.logger
+
 
 async def category_processor(telegram_category, categories, MCategory, alert, sku):
     default_category_ID = default_category_name = default_category_name_en = None
     try:
         for value in categories:
             for item in value:
-                if item['nameTranslated']['ar'] == telegram_category and item['parentId'] == MCategory:
-                    default_category_name = telegram_category
-                    default_category_name_en = item['name']
-                    default_category_ID = item['id']
-                    break
-                else:
-                    continue
-                
+                words = re.split("\s", telegram_category)
+                if telegram_category == item['nameTranslated']['ar']:
+                    if 'parentId' in item:
+                        if item['parentId'] == MCategory:
+                            default_category_name = telegram_category
+                            default_category_name_en = item['name']
+                            default_category_ID = item['id']
+                            break
+                        else:
+                            continue
+                elif telegram_category != item['nameTranslated']['ar']:
+                    for word in words:
+                        if re.search(f"^{word}\s[أ-ي]+|^[أ-ي]+\s{word}", item['nameTranslated']['ar']) and word != '':
+                            if 'parentId' in item:
+                                if item['parentId'] == MCategory:
+                                    default_category_name = item['nameTranslated']['ar']
+                                    default_category_name_en = item['name']
+                                    default_category_ID = item['id']
+                                    break
+                                else:
+                                    continue
+
         if default_category_name:
             logger.info(
                 f"Category processed successfully | Arabic: {default_category_name} | English: {default_category_name_en}")
         else:
             await feedback(settings.session_name, f"Category {telegram_category} is not on the list | Sku: {sku}", 'warning', alert)
-        
+
         main_category_id = int(MCategory)
-             
+
         if default_category_ID == main_category_id or not default_category_ID:
             categories_ids = [main_category_id]
             categories_json = {"id": main_category_id,
@@ -34,12 +50,12 @@ async def category_processor(telegram_category, categories, MCategory, alert, sk
                                "enabled": True}, {"id": default_category_ID,
                                                   "enabled": True}
         logger.info("Category processing is done")
-        
+
     except Exception as e:
         await feedback(settings.session_name, f"Category processing error occurred: {e} | Sku: {sku}", 'exception', alert)
         default_category_ID = main_category_id
         categories_ids = [main_category_id]
         categories_json = {"id": main_category_id,
-                           "enabled": True}    
-    
+                           "enabled": True}
+
     return default_category_name, default_category_name_en, categories_ids, main_category_id, categories_json
